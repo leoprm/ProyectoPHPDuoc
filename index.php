759a439ee76a41e93bcd9d4fd855b9c0fef37339
Leonardo Rivas <leoprm@gmail.com>
2015-06-16 19:52:31 -0400

    no me acuerdo que hice :(

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
         <meta name="author" content="Los 3 Fulanos">
 
         <title>Un Lindo proyecto</title>
-        <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="/assets/dist/css/custom.css" rel="stylesheet">
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/dist/css/custom.css" rel="stylesheet">
 
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
